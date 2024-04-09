6fbadf9b2dc75dd8be4ceec4367dc122d1241580
Yssaky Assad <yssakyassad@gmail.com>
2024-04-08 21:37:25 -0300

    React app finalizado e processo de estilização da pagina iniciado.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import {FiSearch} from 'react-icons/fi';
-import './style.css';
-import {useState} from 'react';
-import api from './services/api';
+import { FiSearch } from "react-icons/fi";
+import "./style.css";
+import { useState } from "react";
+import api from "./services/api";
 
 function App() {
-  const [input, setInput] = useState('')
-  const [Cep, setCep] = useState({});
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
 
-
-  async function handleSearch(){
-
-    if(input === ''){
+  async function handleSearch() {
+    if (input === "") {
       alert("Preencha Algum CEP!");
       return;
     }
@@ -19,35 +17,42 @@ function App() {
       const response = await api.get(`${input}/json`);
       setCep(response.data);
       setInput("");
-    }
-    catch {
+    } catch {
       alert("Ops Erro ao Buscar...");
-      setInput('');
+      setInput("");
     }
   }
 
   return (
     <div className="container">
-      <h1 className="title">Buscador CEP</h1>
+      <h1 className="title">Encontre seu Cep</h1>
 
       <div className="containerInput">
-        <input type="text" 
-        placeholder="Digite seu CEP.."
-        value={input}
-        onChange={(event) => setInput(event.target.value) }
+        <input
+          type="text"
+          placeholder="Digite seu CEP.."
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
         />
         <button className="ButtobSearch" onClick={handleSearch}>
-          <FiSearch size={25} color='#FFF'/>
+          <FiSearch size={25} color="#FFF" />
         </button>
       </div>
 
-      <main className='main'> 
-        <h2>CEP: 07856070</h2>
-        <span>Rua Teste Algum</span>
-        <span>Complemento: Algum Complemento</span>
-        <span>Vila Rosa</span>
-        <span>São Paulo - SP</span>
-      </main>
+      {Object.keys(cep).length > 0 && (
+        <main class="containerCEP">
+          <div class="box">
+            <span class="titleCEP">CEP: {cep.cep}</span>
+
+            <span>{cep.logradouro}</span>
+            <span>Complemento: {cep.complemento}</span>
+            <span>{cep.bairro}</span>
+            <span>
+              {cep.localidade} - {cep.uf}
+            </span>
+          </div>
+        </main>
+      )}
     </div>
   );
 }
